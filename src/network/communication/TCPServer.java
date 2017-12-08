2767138f42f648f98f0b78e897d59e66b8c786a0
Tehemou <tehema.teiti@gmail.com>
2017-12-08 15:21:07 +0100

    1er commit

---
+++ src/network/communication/TCPServer.java
@@ -0,0 +1,16 @@
+package network.communication;
+
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TCPServer {
+
+    private int port;
+
+    public TCPServer(int port) {
+        this.port = port;
+    }
+
+    
+
+}
