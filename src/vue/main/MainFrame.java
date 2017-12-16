a37e7b9940227960addae184f4ac78a3d90d2d8b
Tehemou <tehema.teiti@gmail.com>
2017-12-16 15:31:03 +0100

    Test commit

---
+++ src/vue/main/MainFrame.java
@@ -0,0 +1,23 @@
+package vue.main;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public static final String TITLE = "Système de clavardage";
+
+    public MainFrame() {
+        super(TITLE);
+        setSize(1200,800);
+        setContentPane(new MainPanel());
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainFrame f = new MainFrame();
+    }
+
+}
